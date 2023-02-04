bdbcfb5a8e1e510db174ea0f3012724663d59160
a6888b <ayoubbrahim68@gmail.com>
2023-02-04 22:14:47 +0100

    ajout attribut url, and add code, parameter in set_params

--- sql_injection/request/request_abc.py
+++ sql_injection/request/request_abc.py
@@ -9,9 +9,11 @@ class RequestABC(ABC):
         ABC (ABCMeta): classe abs
     """
     params: dict
+    url: str
 
-    def __init__(self) -> None:
+    def __init__(self, url: str) -> None:
         super().__init__()
+        self.url = url
 
     @abstractmethod
     def request(self) -> requests.Response:
@@ -22,6 +24,12 @@ class RequestABC(ABC):
         """
 
     @abstractmethod
-    def set_params(self) -> None:
+    def set_params(self, data: dict) -> None:
         """Ajoute les parametre dans l'attribut params."""
+
+        for key, value in data.items():
+            if key in self.params:
+                self.params[key] += value
+            else:
+                self.params[key] = value
         
